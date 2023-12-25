bf9a646af321753b0486f24b23ad3a5da51aaa86
Abubakar Mohammed <ASMohammed@student.fullsail.edu>
2023-12-24 20:44:44 -0500

    first commit only grid view and empt grid view file made

---
+++ app/src/main/java/com/example/volumecalculatormath/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.volumecalculatormath;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //each item in the grid view is represented by a layout
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
