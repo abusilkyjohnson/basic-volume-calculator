bd23aaba712e0f8e77ec63b618bd1b09570fdfe8
Abubakar Mohammed <ASMohammed@student.fullsail.edu>
2023-12-25 21:49:05 -0500

    rect prism UI & calculation completed

--- app/src/main/java/com/example/volumecalculatormath/rect_prism_calc_pg.java
+++ app/src/main/java/com/example/volumecalculatormath/rect_prism_calc_pg.java
@@ -3,6 +3,11 @@ package com.example.volumecalculatormath;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class rect_prism_calc_pg extends AppCompatActivity {
 
@@ -10,5 +15,31 @@ public class rect_prism_calc_pg extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rect_prism_calc_pg);
+
+        Button submit = findViewById(R.id.submitButtRectPrism);
+        EditText lengthXML = findViewById(R.id.lengthRectPrism);
+        EditText widthXML = findViewById(R.id.widthRectPrism);
+        EditText heightXML = findViewById(R.id.hieghtRectPrism);
+        TextView resultXML = findViewById(R.id.resultRectPrism);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String lengthInputXmlString = lengthXML.getText().toString();
+                String widthInputXmlString = widthXML.getText().toString();
+                String hieghInputXmlString = heightXML.getText().toString();
+                double lengthInputNum = Double.parseDouble(lengthInputXmlString);
+                double widthInputNum = Double.parseDouble(widthInputXmlString);
+                double heightInputNum = Double.parseDouble(hieghInputXmlString);
+
+                double resultNum = heightInputNum * lengthInputNum * widthInputNum;
+
+                resultXML.setText(" " + resultNum);
+
+                Toast.makeText(rect_prism_calc_pg.this,"The rectangular prism volume is "+ resultNum ,Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
