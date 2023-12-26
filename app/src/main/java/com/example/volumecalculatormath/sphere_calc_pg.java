86af3b599ac2a45665e51246821ddf91004b37b0
Abubakar Mohammed <ASMohammed@student.fullsail.edu>
2023-12-25 20:29:59 -0500

    sphere calculation completed

--- app/src/main/java/com/example/volumecalculatormath/sphere_calc_pg.java
+++ app/src/main/java/com/example/volumecalculatormath/sphere_calc_pg.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class sphere_calc_pg extends AppCompatActivity {
 
@@ -17,12 +18,20 @@ public class sphere_calc_pg extends AppCompatActivity {
 
         int radius;
         Button submit = findViewById(R.id.submitButtSphere);
-        EditText userInput = findViewById(R.id.userInputSphere);
-        TextView result = findViewById(R.id.resultSphere);
+        EditText userInputXml = findViewById(R.id.userInputSphere);
+        TextView resultXML = findViewById(R.id.resultSphere);
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                String userInputXmlString = userInputXml.getText().toString();
+                double userInputNum = Double.parseDouble(userInputXmlString);
+                double resultNum = (4.0/3.0)*(Math.PI)*(Math.pow(userInputNum,3));
+
+                resultXML.setText(" " + resultNum);
+
+                Toast.makeText(sphere_calc_pg.this,"The sphere volume is "+ resultNum ,Toast.LENGTH_SHORT).show();
 
             }
         });
